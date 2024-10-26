1d26d79f3b8b86729cf2aff443c5db16379fb953
bruno monteiro <brunoleomont@gmail.com>
2024-10-25 22:11:46 -0300

    Aula 2

--- src/app/my-first-component/my-first-component.component.ts
+++ src/app/my-first-component/my-first-component.component.ts
@@ -1,12 +1,27 @@
+import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-my-first-component',
+  selector: 'my-first-component',
   standalone: true,
-  imports: [],
+  imports: [NgClass, NgStyle, NgIf, NgFor],
   templateUrl: './my-first-component.component.html',
   styleUrl: './my-first-component.component.css'
 })
 export class MyFirstComponentComponent {
+
+  title = "My First Component #1178";
+
+  name = "Bruno";
+
+  showSuccessMessage = false;
+
+  fator = 3;
+  
+  items = [
+    {name: "bruno", cpf: "123456789", value: 3},
+    {name: "Julia", cpf: "123456789", value: 5},
+    {name: "João", cpf: "123456789", value: 7}
+  ];
 
 }
